e71243278945d824a3d4adda7d6024aeb11a61e2
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-04 13:11:14 -0800

    Add model for items

--- app.py
+++ app.py
@@ -10,6 +10,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret')
 db = SQLAlchemy(app)
 
+# Models
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)       # "Bobby pins"
+    category = db.Column(db.String(50))                    # "Wigs"
+    unit = db.Column(db.String(50))                        # "pcs"
+    count = db.Column(db.Integer, default=0)
+    desc = db.Column(db.Text)
+    low_stock_threshold = db.Column(db.Integer, default=5)
+    logs = db.relationship('CountLog', backref='item', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'category': self.category,
+            'unit': self.unit,
+            'count': self.count,
+            'desc': self.desc,
+            'is_low': self.count <= self.low_stock_threshold
+        }
+
 @app.route("/")
 def home():
     return render_template("index.html")
